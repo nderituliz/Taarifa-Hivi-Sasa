29309e42649f7472a130c774d36730ad9371be79
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-01 14:49:46 +0300

    added a function to process results fetched from our news sources

--- app/requests.py
+++ app/requests.py
@@ -33,3 +33,26 @@ def get_sources():
             print(sources_results_list, '1234')
             
     return sources_results
+
+def process_sources_results(sources_list):
+    """
+    Function  that processes the sources result and transform them to a list of Objects
+    """
+    
+    sources_results = []
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        newsCategory = source_item.get('newsCategory')
+        language = source_item.get('language')
+        country = source_item.get('country')
+        
+        # if poster:
+        source_object = Sources(id, name, description, url, newsCategory, language, country)
+        sources_results.append(source_object)
+
+    print(sources_results)
+
+    return sources_results
