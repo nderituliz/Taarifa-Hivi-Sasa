05e9355f5c04f039dafb129a6f9153a99d599d41
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-01 14:48:22 +0300

    added a function to fetch our new sources

--- app/requests.py
+++ app/requests.py
@@ -13,3 +13,23 @@ def configure_request(app):
     api_key = app.config ["NEWS_API_KEY"]
     base_url = app.config ["NEWS_API_BASE_URL"]
     base_article_url = app.config["ARTICLES_API_BASE_URL"]
+
+def get_sources():
+    """
+    Function that gets the json response to our url request
+    """
+    get_sources_url = base_url
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources_results(sources_results_list)
+            print(sources_results_list, '1234')
+            
+    return sources_results
