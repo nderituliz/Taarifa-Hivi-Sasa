a08f761f047b0b63e92d54e0154ec39904e2ef14
Elizabeth Nderitu <nderituliz@gmail.com>
2020-12-01 14:44:48 +0300

    created an articles class to create our articles objects

--- app/models.py
+++ app/models.py
@@ -12,3 +12,18 @@ class Sources:
         self.language = language
         self.country = country
         
+class Articles:
+    """
+    class to define the news article
+    """
+    
+    def __init__(self,id,title,author,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+        
